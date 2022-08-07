e0c5d2a5970082e47b9c03355c622d66ed9a5411
dymmdaniel <danielmurillomorales01@gmail.com>
2022-08-07 11:52:16 -0500

    first comming

---
+++ src/reto_5/Reto_5.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package reto_5;
+
+/**
+ * Construccion de una aplicacion de escritorio
+ * Daniel Yesid Murillo Morales
+ * Universida Tecnologica Pereira
+ * Reto 5
+ */
+public class Reto_5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola");
+    }
+    
+}
